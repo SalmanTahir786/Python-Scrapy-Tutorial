f03f4d637304bc0d149713f32d6859eb1bcdcbc8
Salman Tahir <salmantahirue@gmail.com>
2022-02-08 20:54:05 +0500

    This is my second commit. Defining the fields in items.py file and then storing the scraped data into it by importing the item.py file into quotes_spider.py file and also creating the instance mean object of class QuotetutorialItem.

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,21 +1,23 @@
 import scrapy
+from ..items import QuotetutorialItem
 
 class QuoteSpider(scrapy.Spider):
-    name = "quotes"  # This is our spider
-
+    name = "quotes"
     start_urls = [
-        'https://quotes.toscrape.com/'  # Do not change the name a these two variables because (scrapy.Spider) class expect these two variables
+        'https://quotes.toscrape.com/'
     ]
-    def parse(self, response):         # self --> (It is self instance or object) response-->It is basically contains the sourse code of our website that we want to scrap
-        # title = response.css('title::text').extract()
-        # yield {'titletext':title}      # Do not change the name of parse function
+    def parse(self, response):
+
+        items = QuotetutorialItem()
         all_div_quotes = response.css("div.quote")
+
         for quotes in all_div_quotes:
             title = quotes.css("span.text::text").extract()
             author = quotes.css(".author::text").extract()
-            tags = quotes.css(".tag::text").extract()
-            yield {
-                "title" : title,
-                "author" : author,
-                "tags" : tags
-            }
+            tag = quotes.css(".tag::text").extract()
+
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
