ff90bcf7d4c8c46d32946c170dcad491bcffbfda
Salman Tahir <salmantahirue@gmail.com>
2022-02-09 12:48:47 +0500

    For using Pipline.py we need to activate this pipline inside our setting.py file. So, Activate it in this step.

--- quotetutorial/quotetutorial/pipelines.py
+++ quotetutorial/quotetutorial/pipelines.py
@@ -7,7 +7,11 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+# Scraped data -> Item Containers -> Json/csv files
+# Scraped data -> Item Containers -> Pipeline -> SQL/Mongo database
 
 class QuotetutorialPipeline:
     def process_item(self, item, spider):
+
+        print("Pipeline :" + item['title'][0])
         return item
